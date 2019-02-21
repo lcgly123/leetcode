225ef3ae4a470a2519b9eae431ca64a782d8a4e4
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-02-21 22:13:40 +0800

    Create 223. Rectangle Area.py

---
+++ 223. Rectangle Area.py
@@ -0,0 +1,35 @@
+223. Rectangle Area
+
+Find the total area covered by two rectilinear rectangles in a 2D plane.
+
+Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+
+Rectangle Area
+
+Example:
+
+Input: A = -3, B = 0, C = 3, D = 4, E = 0, F = -1, G = 9, H = 2
+Output: 45
+
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        """
+        :type A: int
+        :type B: int
+        :type C: int
+        :type D: int
+        :type E: int
+        :type F: int
+        :type G: int
+        :type H: int
+        :rtype: int
+        """
+        total=(C-A)*(D-B)+(G-E)*(H-F)
+        overlap=max(0,min(C,G)-max(A,E))*max(0,min(D,H)-max(B,F))# 有用
+        
+        return total-overlap
+
+
+
+
+
