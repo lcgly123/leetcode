f99fa87ea669eb4a100aefdd3bdf6a2a24e35a2b
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-02-28 18:52:56 +0800

    Create 324. Wiggle Sort II.py

---
+++ 324. Wiggle Sort II.py
@@ -0,0 +1,27 @@
+324. Wiggle Sort II
+
+Given an unsorted array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
+
+Example 1:
+
+Input: nums = [1, 5, 1, 1, 6, 4]
+Output: One possible answer is [1, 4, 1, 5, 1, 6].
+Example 2:
+
+Input: nums = [1, 3, 2, 2, 3, 1]
+Output: One possible answer is [2, 3, 1, 3, 1, 2].
+
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+        half=len(nums[::2])# 偶数位的个数，比//2更好
+        
+        nums[::2],nums[1::2]=nums[:half][::-1],nums[half:][::-1]# NB
+        
+        
+
+
+
