e183f5380a0a587921ce1f8620aac589d5fb3d18
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-03-08 19:17:43 +0800

    Create 396. Rotate Function.py

---
+++ 396. Rotate Function.py
@@ -0,0 +1,43 @@
+396. Rotate Function
+
+Given an array of integers A and let n to be its length.
+
+Assume Bk to be an array obtained by rotating the array A k positions clock-wise, we define a "rotation function" F on A as follow:
+
+F(k) = 0 * Bk[0] + 1 * Bk[1] + ... + (n-1) * Bk[n-1].
+
+Calculate the maximum value of F(0), F(1), ..., F(n-1).
+
+Note:
+n is guaranteed to be less than 105.
+
+Example:
+
+A = [4, 3, 2, 6]
+
+F(0) = (0 * 4) + (1 * 3) + (2 * 2) + (3 * 6) = 0 + 3 + 4 + 18 = 25
+F(1) = (0 * 6) + (1 * 4) + (2 * 3) + (3 * 2) = 0 + 4 + 6 + 6 = 16
+F(2) = (0 * 2) + (1 * 6) + (2 * 4) + (3 * 3) = 0 + 6 + 8 + 9 = 23
+F(3) = (0 * 3) + (1 * 2) + (2 * 6) + (3 * 4) = 0 + 2 + 12 + 12 = 26
+
+So the maximum value of F(0), F(1), F(2), F(3) is F(3) = 26.
+
+class Solution:
+    def maxRotateFunction(self, A: List[int]) -> int:
+        #  F(i) = F(i-1) + sum - n*A[n-i]，
+        sumA=sum(A)
+        
+        f0=0
+        for i,x in enumerate(A):
+            f0+=i*x
+
+        res=[f0]
+        n=len(A)
+        if n==0 or n==1:
+            return 0
+        for i in range(1,n):
+            temp=res[-1]+sumA-n*A[n-i]
+            res.append(temp)
+        
+        print(res)
+        return max(res)
