7f161c02d23d7e63b524d2f61254ffd9f0a8812a
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-05-01 17:08:15 +0800

    Create 787. Cheapest Flights Within K Stops.py

---
+++ 787. Cheapest Flights Within K Stops.py
@@ -0,0 +1,68 @@
+787. Cheapest Flights Within K Stops
+
+There are n cities connected by m flights. Each fight starts from city u and arrives at v with a price w.
+
+Now given all the cities and flights, together with starting city src and the destination dst, your task is to find the cheapest price from src to dst with up to k stops. If there is no such route, output -1.
+
+Example 1:
+Input: 
+n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+src = 0, dst = 2, k = 1
+Output: 200
+Explanation: 
+The graph looks like this:
+
+
+The cheapest price from city 0 to city 2 with at most 1 stop costs 200, as marked red in the picture.
+
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+        
+        # NB
+        dp=[float('inf')]*n
+        dp[src]=0
+        
+        for _ in range(K+1):# k个stop则中间一共k+1个空
+            new_dp=dp[:]# 注意，不然前面改的影响后面的
+            for s,d,cost in flights:
+                new_dp[d]=min(new_dp[d],dp[s]+cost)
+            dp=new_dp[:]
+                
+        return dp[dst] if dp[dst]!=float('inf') else -1
+        
+        
+        
+#         # 可以，但会超时
+#         import collections
+#         gragh=collections.defaultdict(dict)
+        
+#         for u,v,w in flights:
+#             gragh[u][v]=w
+        
+#         money=[None]*n
+#         money[src]=0
+#         k=0
+        
+#         queen=[src]
+#         while(queen):
+#             new=[]
+#             for node in queen:
+#                 for next_node in gragh[node]:
+#                     if next_node!=dst:
+#                         if money[next_node]==None or money[next_node] >money[node]+gragh[node][next_node]:
+#                             money[next_node]=money[node]+gragh[node][next_node]
+#                     else:
+#                         if k<=K+1:
+#                             if money[next_node]==None or money[next_node] >money[node]+gragh[node][next_node]:
+#                                 money[next_node]=money[node]+gragh[node][next_node]
+                        
+                            
+                    
+#                     new.append(next_node)
+#             k+=1
+#             queen=new
+        
+#         return money[dst] if money[dst]!=None else -1
+        
+        
