3f6059e421fb765f0ed55767f9def1516eb7c357
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-05-26 00:33:29 +0800

    Create 89. Gray Code.py

---
+++ 89. Gray Code.py
@@ -0,0 +1,28 @@
+89. Gray Code
+
+The gray code is a binary numeral system where two successive values differ in only one bit.
+
+Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+
+Example 1:
+
+Input: 2
+Output: [0,1,3,2]
+Explanation:
+00 - 0
+01 - 1
+11 - 3
+10 - 2
+
+For a given n, a gray code sequence may not be uniquely defined.
+For example, [0,2,3,1] is also a valid gray code sequence.
+
+00 - 0
+10 - 2
+11 - 3
+01 - 1
+
+class Solution:
+    def grayCode(self, n: 'int') -> 'List[int]':
+        # 格雷码的生成方式        
+        return [(i>>1)^i for i in range(2**n)]
