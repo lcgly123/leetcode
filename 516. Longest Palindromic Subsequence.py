bf9e0e462384c8fd54dd84eff18ebbb4962e9219
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-06-10 16:03:01 +0800

    Update 516. Longest Palindromic Subsequence.py

--- 516. Longest Palindromic Subsequence.py
+++ 516. Longest Palindromic Subsequence.py
@@ -12,42 +12,32 @@ Output:
 
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        #NB
-        def dfs(s,i,j,dp):
+        # NB
+        
+        def dfs(s,i,j):
             if i>j:
                 return 0
             if i==j:
                 return 1
-            if dp[i][j]!=-1:
-                return dp[i][j]# 就是为了防止重复走
-            else:
-                if s[i]==s[j]:
-                    dp[i][j]=2+dfs(s,i+1,j-1,dp)
-                    return dp[i][j]
-                else:
-                    dp[i][j]= max(dfs(s,i+1,j,dp),dfs(s,i,j-1,dp))
-                    return dp[i][j]
+            if (i,j) in memo:
+                return memo[(i,j)]
             
-        dp=[[-1]*len(s) for _ in range(len(s))]
-        res=dfs(s,0,len(s)-1,dp)
-        return res
+            if s[i]==s[j]:
+                res=2+dfs(s,i+1,j-1)
+            else:
+                res=max(dfs(s,i+1,j),dfs(s,i,j-1))
+            memo[(i,j)]=res
+            return res
+        
+        memo={}
+        return dfs(s,0,len(s)-1)
         
         
-        # 可以但会超时
-        # def dfs(s,i,j,dp):
-        #     if i>j:
-        #         return 0
-        #     if i==j:
-        #         # dp[i][j]=1
-        #         return 1
-        #     if s[i]==s[j]:
-        #         # 2+dfs(s,i+1,j-1,dp)
-        #         return 2+dfs(s,i+1,j-1,dp)
-        #     else:
-        #         return max(dfs(s,i+1,j,dp),dfs(s,i,j-1,dp))
-        # dp=[[1]*(len(s)-1) for _ in range(len(s))]
-        # res=dfs(s,0,len(s)-1,dp)
-        # return res
+        
+        
+        
+        
+        
 
 
 
