b5e0edcc2b895fbf4b5b1021a494524a8c29fc28
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-07-26 11:10:01 +0800

    Update 236. Lowest Common Ancestor of a Binary Tree.py

--- 236. Lowest Common Ancestor of a Binary Tree.py
+++ 236. Lowest Common Ancestor of a Binary Tree.py
@@ -24,32 +24,52 @@ Explanation: The LCA of nodes 5 and 1 is 3.
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        
-        def dfs(root,target,path,res):
+        def dfs(root):
             if root==None:
                 return
-            if path[-1]==target:
-                res+=path
+            if root==p or root==q:
+                return root
+            left=dfs(root.left)
+            right=dfs(root.right)
+            if left and right:# 都非空就抛出本节点
+                return root
+            elif left==None:# 输出非空的，实际上是向上传递节点
+                return right
+            elif right==None:
+                return left
+            else:
                 return
-                
-            dfs(root.left,target,path+[root.left],res)
-            if res!=[]:
-                return
-            dfs(root.right,target,path+[root.right],res)
+        return dfs(root)
             
-        res_p=[]
-        dfs(root,p,[root],res_p)
-        res_q=[]
-        dfs(root,q,[root],res_q)
+
+#         def dfs(root,target,path,res):
+#             if root==None:
+#                 return
+#             if path[-1]==target:
+#                 res+=path
+#                 return
+                
+#             dfs(root.left,target,path+[root.left],res)
+#             if res!=[]:
+#                 return
+#             dfs(root.right,target,path+[root.right],res)
+            
+#         res_p=[]
+#         dfs(root,p,[root],res_p)
+#         res_q=[]
+#         dfs(root,q,[root],res_q)
         
-        # print(res_p)
-        # print(res_q)
-        m=len(res_p)
-        n=len(res_q)
-        for i in range(min(m,n)):
-            if res_p[i].val!=res_q[i].val:
-                return res_p[i-1]
-        return res_p[-1] if m<n else res_q[-1]
+#         # print(res_p)
+#         # print(res_q)
+#         m=len(res_p)
+#         n=len(res_q)
+#         for i in range(min(m,n)):
+#             if res_p[i].val!=res_q[i].val:
+#                 return res_p[i-1]
+#         return res_p[-1] if m<n else res_q[-1]
+        
+        
+        
         
         
         
