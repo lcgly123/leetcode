b3e0140d06a7e22c2b691c1847f7b2485736001a
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-07-28 12:47:19 +0800

    Update 287. Find the Duplicate Number.py

--- 287. Find the Duplicate Number.py
+++ 287. Find the Duplicate Number.py
@@ -13,13 +13,46 @@ Output: 3
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        # 见过好多次了
-        from collections import defaultdict
+        # 根据鸽笼原理，给定n + 1个范围[1, n]的整数，其中一定存在数字出现至少两次。
+
+        # 假设枚举的数字为 n / 2：
+
+        # 遍历数组，若数组中不大于n / 2的数字个数超过n / 2，则可以确定[1, n /2]范围内一定有解，
+
+        # 否则可以确定解落在(n / 2, n]范围内。
         
-        dic=defaultdict(lambda :0)
-        for i in nums:
-            dic[i]+=1
+        l,r=1,len(nums)# 因为题目给定的数字是1-n
+        while(l<=r):
+            mid=(l+r)//2 # 中间数
+            add=sum([x<=mid for x in nums])
             
-        for k,v in dic.items():
-            if v>1:
-                return k
+            if add>mid:
+                r=mid-1
+            else:
+                l=mid+1
+        
+        return l# 得是l
+        
+
+        
+        
+#         # 见过好多次了
+#         from collections import defaultdict
+        
+#         dic=defaultdict(lambda :0)
+#         for i in nums:
+#             dic[i]+=1
+            
+#         for k,v in dic.items():
+#             if v>1:
+#                 return k
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
