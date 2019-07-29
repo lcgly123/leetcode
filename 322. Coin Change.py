d16ee544c8150ff2f532eb79294c80c8d7a0e370
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-07-29 11:18:34 +0800

    Update 322. Coin Change.py

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -15,79 +15,33 @@ Output: -1
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        res=[float('inf')]*(amount+1)
-        res[0]=0
+        dp=[float('inf')]*(amount+1)
+        dp[0]=0
+        # 组合
+        for c in coins:
+            for i in range(1,amount+1):
+                if i-c>=0:
+                    dp[i]=min(dp[i],dp[i-c]+1)
+        
+        return dp[-1] if dp[-1]!=float('inf') else -1
+        
+        
+        
+        # 排列
+#         res=[float('inf')]*(amount+1)
+#         res[0]=0
         
      
         
-        for i in range(1,amount+1):
-            for c in coins:
-                if i>=c:
-                    res[i]=min(res[i],res[i-c]+1)# 就是如果这个钱-c是可以凑出来的，那这个也可以，如果不可以，那它也可以
+#         for i in range(1,amount+1):
+#             for c in coins:
+#                 if i>=c:
+#                     res[i]=min(res[i],res[i-c]+1)# 就是如果这个钱-c是可以凑出来的，那这个也可以，如果不可以，那它也可以
                 
-        if res[amount]==float('inf'):
-            return -1
-        else:
-            return res[amount]
-        
-        
-        
-        
-        
-        
-        # 可以但会超时
-        
-#         def dfs(candidates,target,path,res):
-#             if path!=[]:
-#                 if sum(path)==target:
-#                     return res.append(len(path))
-#                 if sum(path)>target:
-#                     return 
-                
-#             for i in range(len(candidates)):
-#                 dfs(candidates,target,path+[candidates[i]],res)
-   
-#         res=[]
-#         dfs(coins,amount,[],res)
-#         if amount==0:
-#             return 0
-#         if res!=[]:
-#             return min(res)
-#         else:
+#         if res[amount]==float('inf'):
 #             return -1
-
-# # 可以但会内存超出限制
-#         def bfs(queen,target):
-#             queen=queen
-#             path=[[x] for x in queen]
-            
-#             while(queen):
-#                 new_layer=[]
-#                 new_path=[]
-                
-                 
-                
-#                 for i in range(len(queen)):
-#                     if sum(path[i])==target:
-#                         return len(path[i])
-#                     if sum(path[i])>target:
-#                         continue
-                    
-#                     for node in queen:
-#                         new_layer.append(node)
-#                         new_path.append(path[i]+[node])
-                
-#                 queen=new_layer
-#                 path=new_path
-                
-#         if amount==0:
-#             return 0
-#         res=bfs(coins,amount)
-#         if res!=None:
-#             return res
 #         else:
-#             return -1
-                
-                    
-                    
-            
+#             return res[amount]
+        
+        
+        
