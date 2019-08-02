11b90b47bc069f582da8efa001bc5a78d21157d6
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-08-02 22:03:00 +0800

    Create 392. Is Subsequence.py

---
+++ 392. Is Subsequence.py
@@ -0,0 +1,37 @@
+392. Is Subsequence
+
+Given a string s and a string t, check if s is subsequence of t.
+
+You may assume that there is only lower case English letters in both s and t. t is potentially a very long (length ~= 500,000) string, and s is a short string (<=100).
+
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+s = "abc", t = "ahbgdc"
+
+Return true.
+
+Example 2:
+s = "axc", t = "ahbgdc"
+
+
+
+# 判断一个字符串是不是另一个字符串的子列（不一定连续，但要保持顺序）
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if s=='':
+            return True
+        index=0
+        for c in t:
+            if index==len(s):
+                return True
+            if c==s[index]:
+                index+=1
+        return index==len(s)
+        
+        
