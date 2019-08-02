fc8b8a1f3b5dff8864d795f6de5e986cc8631024
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-08-02 23:21:25 +0800

    Update 402. Remove K Digits.py

--- 402. Remove K Digits.py
+++ 402. Remove K Digits.py
@@ -21,20 +21,38 @@ class Solution:
         
         if len(num)==1:
             return '0'
-        while(k!=0):
+        num=list(map(int,num))
+        
+        while(k>0):
             k-=1
             
-            for i in range(len(num)-1):
-                if num[i]>num[i+1]:# 只要把这种删了就行，好像见过
-                    num_=list(num)
-                    num_.pop(i)
-                    num=''.join(num_)
+            for i in range(len(num)-1):# 所以这是应对for循环完全不循环的技巧
+                if num[i]>num[i+1]:
+                    num.pop(i)
                     break
-            else:# 循环完会执行这个，break就不会
-                num_=list(num)
-                num_.pop()
-                num=''.join(num_)
+            else:# 循环完会执行这个，break就不会,如果没有，就pop最后一个(必须这样，不然如果全删的话，最后一个删不掉)
+                num.pop()
         if len(num)==0:
             return '0'
-        return str(int(num))
+        return str(int(''.join(list(map(str,num)))))
+        
+
+#         if len(num)==1:
+#             return '0'
+#         while(k!=0):
+#             k-=1
+            
+#             for i in range(len(num)-1):
+#                 if num[i]>num[i+1]:# 只要把这种删了就行，好像见过，不懂为啥
+#                     num_=list(num)
+#                     num_.pop(i)
+#                     num=''.join(num_)
+#                     break
+#             else:# 循环完会执行这个，break就不会,如果没有，就pop最后一个
+#                 num_=list(num)
+#                 num_.pop()
+#                 num=''.join(num_)
+#         if len(num)==0:
+#             return '0'
+#         return str(int(num))
             
