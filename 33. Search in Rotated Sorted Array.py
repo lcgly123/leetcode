7f7df2b1b9d1403a984b536d872188c211394b46
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-08-04 16:03:41 +0800

    Update 33. Search in Rotated Sorted Array.py

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -29,79 +29,59 @@ class Solution:
         # 如果都没有，就是正常的二分查找
         
         # 二分查找可以处理从任意多元素，包括0元素
-     
-  
+        
+        
+        
         l,r=0,len(nums)-1
         while(l<=r):
             mid=(l+r)//2
-            print(nums[l],nums[mid],nums[r])
-            if nums[mid]==target:
+            
+            if nums[mid]==target:# 这个必须在上面，二分法这个都要在上面
                 return mid
-            if nums[l]>nums[mid]:# 因为本题的初始排列一定是从小到大的，所以包含转折等价于nums[l]>nums[mid]
-                if (target>=nums[l] or target<nums[mid]):# 不会等于nums[mid],但有可能等于nums[l]
-                    r=mid-1
-                elif nums[l]>nums[mid]:
-                    l=mid+1
-            elif nums[mid]>nums[r]:
-                if (target>nums[mid] or target<=nums[r]):# 不会等于nums[mid],但有可能等于nums[r]
-                    l=mid+1
-                elif nums[mid]>nums[r]:
-                    r=mid-1
-            else:
-                if target<nums[mid]:
+            if nums[l]>nums[mid]:
+                if target>=nums[l] or target<nums[mid]:
                     r=mid-1
                 else:
                     l=mid+1
-        
-        return  -1
+            elif nums[r]<nums[mid]:
+                if target<=nums[r] or target>nums[mid]:
+                    l=mid+1
+                else:
+                    r=mid-1
+            else:
+                if target>nums[mid]:
+                    l=mid+1
+                elif target<nums[mid]:
+                    r=mid-1
                 
-                
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         if not nums or len(nums) == 0:
-#             return -1
-            
-#         l,r = 0,len(nums)-1
+               
+        return -1
+                    
 
-#         while(l < r-1):# 确保能从循环里出来
-#             mid = (l+r)//2
-#             if nums[mid] < nums[r]:# 正序
-#                 if nums[mid]<target and nums[r]>=target:# 单调区间
-#                     l=mid
-#                 elif nums[mid]==target:
-#                     return mid
+  
+         #l,r=0,len(nums)-1
+#         while(l<=r):
+#             mid=(l+r)//2
+#             print(nums[l],nums[mid],nums[r])
+#             if nums[mid]==target:
+#                 return mid
+#             if nums[l]>nums[mid]:# 这不能有= # 因为本题的初始排列一定是从小到大的，所以包含转折等价于nums[l]>nums[mid]
+#                 if (target>=nums[l] or target<nums[mid]):# 不会等于nums[mid],但有可能等于nums[l]
+#                     r=mid-1
+#                 elif nums[l]>nums[mid]:
+#                     l=mid+1
+#             elif nums[mid]>nums[r]:
+#                 if (target>nums[mid] or target<=nums[r]):# 不会等于nums[mid],但有可能等于nums[r]
+#                     l=mid+1
+#                 elif nums[mid]>nums[r]:
+#                     r=mid-1
+#             else:
+#                 if target<nums[mid]:
+#                     r=mid-1
 #                 else:
-#                     r=mid
-#             else:# 倒序
-#                 if nums[mid]>target and nums[l]<=target:# 单调区间
-#                     r=mid
-#                 elif nums[mid]==target:
-#                     return mid
-#                 else:
-#                     l=mid 
-            
-#         # 因为从while出来会剩下两个
-#         if nums[l] == target:
-#             return l
-#         elif nums[r] == target:
-#             return r
-#         return -1
+#                     l=mid+1
         
+#         return  -1
+                
+                
         
