dff13853156c19309ff7cb96de37dae77dcb066f
lcgly123 <42167671+lcgly123@users.noreply.github.com>
2019-08-08 18:55:20 +0800

    Update 486. Predict the Winner.py

--- 486. Predict the Winner.py
+++ 486. Predict the Winner.py
@@ -19,28 +19,59 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        from collections import defaultdict
-        memo=defaultdict(lambda :-1)# memo只是为了遇到搜过的是快速返回，没别的用，只是为了防止超时
-        # 很有意思，但并不难
-        def dfs(nums,i,j,memo):
+        
+#         dp=[[0]*len(nums) for _ in range(len(nums))]
+#         for i in range(len(nums)):
+#             dp[i][i]=nums[i]
+        
+#         for s in range(len(nums)-2,-1,-1):
+#             for e in range(s+1,len(nums)):
+#                 chooseleft=nums[s]-dp[s+1][e]
+#                 chooseright=nums[e]-dp[s][e-1]
+#                 dp[s][e]=max(chooseleft,chooseright)
+        
+#         return dp[0][-1]>=0
+
+        
+        def dfs(nums,i,j):
             if i>j:
-                return 0 # 也可以
+                return 0
             if i==j:
                 return nums[i]
-            if memo[(i,j)]!=-1:
+            if (i,j) in memo:
                 return memo[(i,j)]
+            
+            score=max(nums[i]+min(dfs(nums,i+2,j),dfs(nums,i+1,j-1)),
+                     nums[j]+min(dfs(nums,i,j-2),dfs(nums,i+1,j-1)))
+            memo[(i,j)]=score
+            return score
+        memo={}
+        s=dfs(nums,0,len(nums)-1)
+        return s>=sum(nums)-s
+        
+
+#         from collections import defaultdict
+#         memo=defaultdict(lambda :-1)# memo只是为了遇到搜过的是快速返回，没别的用，只是为了防止超时
+#         # 很有意思，但并不难
+#         def dfs(nums,i,j,memo):
+#             if i>j:
+#                 return 0 # 也可以
+#             if i==j:
+#                 return nums[i]
+#             if memo[(i,j)]!=-1:
+#                 return memo[(i,j)]
      
             
-            score=max(nums[i]+min(dfs(nums,i+2,j,memo),dfs(nums,i+1,j-1,memo)),# dfs（）代表搜索i-j时（先拿）最多得分，min是因为第二个拿的一定拿最大的，所以下一次只会是两者最小的
-                     nums[j]+min(dfs(nums,i,j-2,memo),dfs(nums,i+1,j-1,memo)))
-            memo[(i,j)]=score
-            return memo[(i,j)]
+#             score=max(nums[i]+min(dfs(nums,i+2,j,memo),dfs(nums,i+1,j-1,memo)),# dfs（）代表搜索i-j时（先拿）最多得分，min是因为第二个拿的一定拿最大的，所以下一次只会是两者最小的
+#                      nums[j]+min(dfs(nums,i,j-2,memo),dfs(nums,i+1,j-1,memo)))
+#             memo[(i,j)]=score
+#             return memo[(i,j)]
         
-        total=sum(nums)
-        score=dfs(nums,0,len(nums)-1,memo)
-        print(score)
+#         total=sum(nums)
+#         score=dfs(nums,0,len(nums)-1,memo)
+#         print(score)
         
-        return score>=total-score
+#         return score>=total-score
             
         
         
